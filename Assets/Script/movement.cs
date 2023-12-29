74d882b3497e10da9ef947747f6808a1d4124b27
MateGiorgelashvili <146206302+MateGiorgelashvili@users.noreply.github.com>
2023-12-29 22:40:44 +0400

    Update movement.cs

--- Assets/Script/movement.cs
+++ Assets/Script/movement.cs
@@ -4,13 +4,44 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
+    Rigidbody2D rb;
+    [SerializeField] float moveSpeed = 8f;
+    [SerializeField] float jumpForce = 3f;
+    public Vector2 boxSize = new Vector2(1, 0.2f);
+    public float castDistance = 1;
+    public LayerMask groundLayer;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
+    // Update is called once per frame
     void Update()
     {
-        
+        float movementInput = Input.GetAxisRaw("Horizontal");
+        Vector2 jump = new Vector2(0, jumpForce * 100);
+
+        transform.Translate(movementInput * Time.deltaTime * moveSpeed, 0, 0);
+
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded())
+        {
+            Debug.Log(1);
+            rb.AddForce(jump);
+        }
+    }
+
+    public bool isGrounded()
+    {
+        if(Physics2D.BoxCast(transform.position, boxSize, 0, -transform.up, castDistance, groundLayer))
+        {
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position-transform.up * castDistance, boxSize);
     }
 }
