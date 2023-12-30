90151de794d8c1a68e42437ec21105bc4a7d0cd4
Giga30 <129112606+Giga30@users.noreply.github.com>
2023-12-30 21:52:03 +0400

    updated trap

--- Assets/Script/trap.cs
+++ Assets/Script/trap.cs
@@ -5,15 +5,23 @@ using UnityEngine;
 public class trap : MonoBehaviour
 {
     [SerializeField] private GameObject trap1;
+    [SerializeField] private GameObject snowDisapear;
     AudioSource audioSource;
     void Update()
     {
         audioSource = GetComponent<AudioSource>();
+
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        trap1.GetComponent<Collider2D>().isTrigger = true;
-        audioSource.Play();
+        GameObject.Destroy(trap1);
+        GameObject.Destroy(snowDisapear);
+        Invoke("batoninika", 0.8f);
 
+    }
+
+    void batoninika()
+    {
+        audioSource.Play();
     }
 }
