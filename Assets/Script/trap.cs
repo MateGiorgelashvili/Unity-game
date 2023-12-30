ce86b5248f0a03e50f8e5b399942f7e57b04864f
Giga30 <129112606+Giga30@users.noreply.github.com>
2023-12-30 21:24:43 +0400

    alat
    organized files, and added sfx to fall and street lamp upd (the greatest update of this decade) maniga

--- Assets/Script/trap.cs
+++ Assets/Script/trap.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class trap : MonoBehaviour
 {
     [SerializeField] private GameObject trap1;
+    AudioSource audioSource;
     void Update()
     {
-        
+        audioSource = GetComponent<AudioSource>();
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         trap1.GetComponent<Collider2D>().isTrigger = true;
+        audioSource.Play();
+
     }
 }
