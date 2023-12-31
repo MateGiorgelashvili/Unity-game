ab52d050954139c6557db5f8414701d6f90bd79f
Giga30 <129112606+Giga30@users.noreply.github.com>
2023-12-31 15:08:29 +0400

    fixed collision
    maniga

--- Assets/Script/movement.cs
+++ Assets/Script/movement.cs
@@ -7,60 +7,59 @@ public class movement : MonoBehaviour
     Rigidbody2D rb;
     [SerializeField] float moveSpeed = 8f;
     [SerializeField] float jumpForce = 3f;
+
     public Vector2 boxSize = new Vector2(1, 0.2f);
-    public float castDistance = 1;
+    public float castDistance = 0.65f;
     public LayerMask groundLayer;
+
+    public Vector2 boxSize1 = new Vector2(1, 0.2f);
+    public float castDistance1 = 0.65f;
+    public LayerMask groundLayer1;
+
+    public Vector2 boxSize2 = new Vector2(1, 0.2f);
+    public float castDistance2 = 0.65f;
+    public LayerMask groundLayer2;
+
     private bool isFacingRight = true;
     Animator Anim;
-
-    public int maxJumps = 2;
-    public int jumpsLeft;
-
-    bool isIdle = true;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         Anim = GetComponent<Animator>();
-        jumpsLeft = maxJumps;
     }
 
+    // Update is called once per frame
     void Update()
     {
+
+
         float movementInput = Input.GetAxisRaw("Horizontal");
+
+        if(isTouchingWallL() && movementInput < 0)
+        {
+            movementInput = 0f;
+        }
+
+        if (isTouchingWallR() && movementInput > 0)
+        {
+            movementInput = 0f;
+        }
 
         if (movementInput != 0)
         {
             Anim.Play("run");
-            isIdle = false;
         }
-        else if (isGrounded() && !isIdle)
-        {
-            Anim.Play("idle");
-            isIdle = true;
-        }
+
 
         Vector2 jump = new Vector2(0, jumpForce * 100);
 
         transform.Translate(movementInput * Time.deltaTime * moveSpeed, 0, 0);
 
-        if (Input.GetKeyDown(KeyCode.Space) && (isGrounded() || jumpsLeft > 0))
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded())
         {
-            rb.velocity = new Vector2(rb.velocity.x, 0);
             rb.AddForce(jump);
-            jumpsLeft--;
-
-            if (!isGrounded())
-            {
-                isIdle = true;
-            }
         }
-
-        if (isGrounded())
-        {
-            jumpsLeft = maxJumps;
-        }
-
         if (movementInput > 0 && !isFacingRight || movementInput < 0 && isFacingRight)
         {
             Flip();
@@ -79,9 +78,35 @@ public class movement : MonoBehaviour
         }
     }
 
+    public bool isTouchingWallL()
+    {
+        if (Physics2D.BoxCast(transform.position, boxSize1, 0, -transform.right, castDistance1, groundLayer1))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public bool isTouchingWallR()
+    {
+        if (Physics2D.BoxCast(transform.position, boxSize2, 0, transform.right, castDistance2, groundLayer2))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
     private void OnDrawGizmos()
     {
         Gizmos.DrawWireCube(transform.position - transform.up * castDistance, boxSize);
+        Gizmos.DrawWireCube(transform.position - -transform.right * castDistance1, boxSize1);
+        Gizmos.DrawWireCube(transform.position - transform.right * castDistance2, boxSize2);
     }
 
     void Flip()
